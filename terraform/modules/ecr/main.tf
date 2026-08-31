939cbf05416ae92773d0874010e4553950ec357a
rlfrjs3 <rlfrjs3@naver.com>
2026-08-31 10:45:54 +0900

    aaa

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -4,6 +4,7 @@ resource "aws_ecr_repository" "app" {
   image_scanning_configuration { scan_on_push = true }
 
   image_tag_mutability = "MUTABLE"
+  force_delete         = true #ECR에 이미지가 남아있어도 강제 삭제
 
   tags = { Name = "${var.project_name}-app" }
 }
