8c28db83323b4ef053cb6b1442c26bcb15d7d282
rlfrjs3 <rlfrjs3@naver.com>
2026-09-08 10:48:53 +0900

    shop-app_Add

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,12 +1,24 @@
-resource "aws_ecr_repository" "app" {
-  name = "${var.project_name}-app"
+resource "aws_ecr_repository" "flask" {
+  name = "${var.project_name}-flask"
 
   image_scanning_configuration { scan_on_push = true }
 
   image_tag_mutability = "MUTABLE"
   force_delete         = true #ECR에 이미지가 남아있어도 강제 삭제
 
-  tags = { Name = "${var.project_name}-app" }
+  tags = { Name = "${var.project_name}-flask" }
 }
+
+resource "aws_ecr_repository" "shop" {
+  name = "${var.project_name}-shop"
+
+  image_scanning_configuration { scan_on_push = true }
+
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true #ECR에 이미지가 남아있어도 강제 삭제
+
+  tags = { Name = "${var.project_name}-shop" }
+}
+
 
 
--- terraform/modules/ecr/outputs.tf
+++ terraform/modules/ecr/outputs.tf
@@ -1,1 +1,2 @@
-output "repository_url" { value = aws_ecr_repository.app.repository_url }
+output "flask_repository_url" { value = aws_ecr_repository.flask.repository_url }
+output "shop_repository_url" { value = aws_ecr_repository.shop.repository_url }
