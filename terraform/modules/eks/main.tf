ca9a90c4165735e8039d31e5a5337d4e0521cebb
rlfrjs3 <rlfrjs3@naver.com>
2026-09-11 12:10:30 +0900

    Modify Terraform EKS Module

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -20,8 +20,8 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids    = var.private_subnet_ids
 
   scaling_config {
-    desired_size = 2
-    min_size     = 2
+    desired_size = 4
+    min_size     = 3
     max_size     = 4
   }
 
